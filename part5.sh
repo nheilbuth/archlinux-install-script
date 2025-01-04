85488e1489401848cd4c96b48833cee77b674c0e
Nicolai Heilbuth <nheilbuth@hotmail.com>
2025-01-04 10:57:16 +0100

    Update part5.sh

--- part5.sh
+++ part5.sh
@@ -3,10 +3,8 @@ sudo pacman -S docker-compose
 sudo usermod -aG docker heilbuth
 newgrp docker
 sudo systemctl enable docker
-
 sudo pacman -S zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 sudo pacman -S k9s
 sudo pacman -S kdiff3
 sudo pacman -S btop
@@ -16,7 +14,6 @@ sudo systemctl enable ufw
 yay -S auto-cpufreq
 sudo systemctl enable auto-cpufreq
 sudo pacman -S timeshift
-
 yay -S howdy
 yay -S powershell-bin
 pacman -S dotnet-sdk
