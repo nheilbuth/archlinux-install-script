39397d6e3798497e3b36b1bd497ae05b63ff3b84
Nicolai Heilbuth <nheilbuth@hotmail.com>
2025-01-04 10:55:20 +0100

    Create part5.sh

---
+++ part5.sh
@@ -0,0 +1,24 @@
+sudo pacman -S docker
+sudo pacman -S docker-compose
+sudo usermod -aG docker heilbuth
+newgrp docker
+sudo systemctl enable docker
+
+sudo pacman -S zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sudo pacman -S k9s
+sudo pacman -S kdiff3
+sudo pacman -S btop
+sudo pacman -S networkmanager-openvpn
+sudo pacman -S ufw
+sudo systemctl enable ufw
+yay -S auto-cpufreq
+sudo systemctl enable auto-cpufreq
+sudo pacman -S timeshift
+
+yay -S howdy
+yay -S powershell-bin
+pacman -S dotnet-sdk
+pacman -S aspnet-runtime
+pacman -S azure-cli
