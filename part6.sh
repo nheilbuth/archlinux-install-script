3fb7fff9347c7656446a41f962abeeb572e92893
Nicolai Heilbuth <nheilbuth@hotmail.com>
2025-10-01 08:05:34 +0200

    Update part6.sh

--- part6.sh
+++ part6.sh
@@ -13,8 +13,8 @@ sudo systemctl enable ufw
 yay -S auto-cpufreq
 sudo systemctl enable auto-cpufreq
 sudo pacman -S timeshift
-yay -S howdy
+#yay -S howdy
 yay -S powershell-bin
-pacman -S dotnet-sdk
-pacman -S aspnet-runtime
-pacman -S azure-cli
+#pacman -S dotnet-sdk
+#pacman -S aspnet-runtime
+#pacman -S azure-cli
